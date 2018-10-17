a03f2598571e6ebf32a0d68d9ab5244e11735b66
U-ZONK\oleg <you@example.com>
2018-10-17 17:06:05 +0400

    z

--- src/cpp_share/util/def_heli0.cpp
+++ src/cpp_share/util/def_heli0.cpp
@@ -4,24 +4,24 @@
 
 std::string heli_impl() { return "0"; }
 
-HeliNativeBit::HeliNativeBit(cHeliNativeEvalKey ek): p(new HeliCtxt) {}
+HeliNativeBit::HeliNativeBit(cHeliNativeEvalKey ek): ctxt(new HeliCtxt) {}
 
 HeliNativeBit::HeliNativeBit(const std::string & enc, cHeliNativeEvalKey ek)
     : HeliNativeBit(ek)
 {
-    if ( enc == "0" ) p->b = false;
-    else if ( enc == "1" ) p->b = true;
+    if ( enc == "0" ) ctxt->b = false;
+    else if ( enc == "1" ) ctxt->b = true;
     else throw "Bad init in HeliNativeBit [" + enc + "]";
 }
 
 std::string HeliNativeBit::str(cHeliNativeEvalKey ek) const
 {
-    return p->b ? "1" : "0";
+    return ctxt->b ? "1" : "0";
 }
 
 HeliNativeBit::HeliNativeBit(const HeliNativeBit & b, cHeliNativeEvalKey ek)
     : HeliNativeBit(ek)
 {
-    p->b = b.p->b;
+    ctxt->b = b.ctxt->b;
 }
 
